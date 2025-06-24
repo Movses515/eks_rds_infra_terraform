79a05e851fa1a5963d66ef3b6fc99de54ad4f3d6
Movses <mmartirosyan69@gmail.com>
2025-06-24 16:04:03 +0400

    added ingress

---
+++ ingress.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "nginx_ingress" {
+  name       = "ingress-nginx"
+  chart      = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  namespace  = "ingress-nginx"
+  create_namespace = true
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+}
